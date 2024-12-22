f487138a2c72e0d889d60d5ad82518b7639e919b
adilsontchameia <adilsontchameia@gmail.com>
2024-12-22 14:07:07 +0100

    ref: moved code to the own targer

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,28 +1,9 @@
 import 'package:faker/faker.dart';
+import 'package:flutter_tdd_bdd_solid_clean_arch/data/http/http.dart';
+import 'package:flutter_tdd_bdd_solid_clean_arch/data/usecases/usecases.dart';
 import 'package:flutter_tdd_bdd_solid_clean_arch/domain/usecases/usecases.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
-
-  RemoteAuthentication({required this.httpClient, required this.url});
-
-  Future<void> auth(AuthenticationParams params) async {
-    final body = {'email': params.email, 'password': params.secret};
-
-    await httpClient.request(url: url, method: 'post', body: body);
-  }
-}
-
-abstract class HttpClient {
-  Future<void> request({
-    required String url,
-    required String method,
-    Map body,
-  });
-}
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
