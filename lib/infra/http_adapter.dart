580fd6958fe626c3453b2b523c1cf4ce904b93f9
adilsontchameia <adilsontchameia@gmail.com>
2024-12-26 15:44:01 +0100

    test: Should return ServerError if post throws

--- lib/infra/http_adapter.dart
+++ lib/infra/http_adapter.dart
@@ -22,12 +22,16 @@ class HttpAdapter implements HttpClient {
     final jsonBody = data != null ? jsonEncode(data) : null;
     var response = Response(requestOptions: RequestOptions(), statusCode: 500);
 
-    if (method == 'post') {
-      response = await client.post(
-        url,
-        options: Options(headers: headers),
-        data: jsonBody,
-      );
+    try {
+      if (method == 'post') {
+        response = await client.post(
+          url,
+          options: Options(headers: headers),
+          data: jsonBody,
+        );
+      }
+    } catch (error) {
+      throw HttpError.serverError;
     }
 
     return _handleResponse(response);
